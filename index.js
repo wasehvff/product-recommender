a0f274bffbdd21152957b0f8a26890cfaef8905f
wasehvff <129386449+wasehvff@users.noreply.github.com>
2023-05-03 16:54:19 +0500

    added api for product-recommendation

---
+++ index.js
@@ -0,0 +1,58 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import cors from 'cors'
+import { OpenAI } from "langchain/llms/openai";
+import { DataSource } from "typeorm";
+import { SqlDatabase } from "langchain/sql_db";
+import { SqlDatabaseChain } from "langchain/chains";
+
+
+const app = express()
+const port = process.env.PORT || 4000
+app.use(express.json())
+app.use(cors())
+
+let datasource
+try {
+  
+ datasource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "postgres",
+    database: "products",
+  });
+} catch (error) {
+  console.log(error)
+}
+
+const db = await SqlDatabase.fromDataSourceParams({
+  appDataSource: datasource,
+});
+
+const chain = new SqlDatabaseChain({
+  llm: new OpenAI({ openAIApiKey :process.env.OPENAI_API_KEY , temperature: 0 }),
+  database: db,
+});
+
+app.post('/product_recommend', async (req, res) => {
+  try {
+  const { prompt } = req.body
+  const result = await chain.run(prompt);
+  console.log(result);
+
+
+  res.json({ 
+  message : result
+})
+
+} catch (error) {
+res.json({error})       
+}
+})
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`)
+})
